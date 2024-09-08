c27ddf7d903a63c14e6ec43e88449437b68362b3
Ildar Fakhrtdinov <test1@hello.world>
2024-09-08 16:47:58 +0300

    Добавлена арифметика

--- src/Main.java
+++ src/Main.java
@@ -3,8 +3,14 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("Введите текст и нажмите Enter");
-        String text = new Scanner(System.in).nextLine();
-        System.out.println("Длина текста: " + text.length());
+        System.out.println("Введите число 1 и нажмите Enter");
+        int number1 = new Scanner(System.in).nextInt();
+        System.out.println("Введите число 2 и нажмите Enter");
+        int number2 = new Scanner(System.in).nextInt();
+        System.out.println("Сумма: " + (number2+number1));
+        System.out.println("Разность: " + (number1-number2));
+        System.out.println("Произведение: " + (number1*number2));
+        System.out.println("Частное: " + ((double)number1/number2));
+
     }
 }
